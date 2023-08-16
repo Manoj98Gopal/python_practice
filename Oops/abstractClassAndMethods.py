89978886f5b3566183a3e3b1d091926b4d0142a5
manoj98gopal <manoj98gopal@gmail.com>
2023-08-16 22:08:58 +0530

    abstract class and method added

--- Oops/abstractClassAndMethods.py
+++ Oops/abstractClassAndMethods.py
@@ -1,1 +1,27 @@
-abstractClassAndMethods.py
+
+# abstract method means  , a method doesn't have body is called abstract method and  a class have abstract method is called abstract class
+# An abstract method is a method declared in a base class (abstract class) but without any implementation. 
+# It's meant to be overridden by the subclasses. An abstract class is a class that cannot be instantiated
+# directly and is meant to be subclassed to provide concrete implementations for its abstract methods.
+
+from abc import ABC, abstractmethod
+
+
+class student:
+
+	@abstractmethod
+	def greet(self):
+		pass
+
+class std(student):
+
+	def greet(self):
+		print("i am printing")
+
+
+s = std()
+
+s.greet()
+
+
+
