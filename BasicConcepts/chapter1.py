76ca7a0cd2103f220c3593fee20dc8e4e6339ee3
manoj98gopal <manoj98gopal@gmail.com>
2024-02-29 22:04:03 +0530

    basic python variables and data types

---
+++ BasicConcepts/chapter1.py
@@ -0,0 +1,33 @@
+
+# variables
+
+age = 25            #integer
+name = "Manoj G"    #string
+is_Coder = True     #boolean
+
+
+print("age is ===",age)
+print("name is ===",name)
+print("proffestion is  ===",is_Coder)
+
+
+#Data types
+
+age = 44                               #Integer       :-  Represents whole numbers without any decimal points.
+height = 6.1                           #Float value   :-  Represents numbers with decimal points.
+name = "Manoj Achar"                   #String        :-  Represents text data, enclosed within single or double quotes.
+is_Dancer = False                      #Boolean       :-  Represents truth values, True or False.
+numbers = [1,2,3,4,5,6]                #list          :-  Represents an ordered collection of elements. Elements can be of different data types.
+cordinates = (3,5)                     #tuple         :-  similar to lists, but immutable (cannot be modified after creation).
+person = {'name':'Manoj','age':'25'}   #dictionary    :-  Represents a collection of key-value pairs.
+unique_number = {1,2,3,4,5}            #Set           :-  Represents an unordered collection of unique elements.
+
+print("This is interger ===",age)
+print ("This is float",height)
+print("This is string ===",name)
+print("This is Boolean ===",is_Dancer)
+print("This is list ===",numbers)
+print("This is tuple ===",cordinates)
+print("This is dictionary ==== ",person)
+print("THis is sets ===",type(unique_number))
+
