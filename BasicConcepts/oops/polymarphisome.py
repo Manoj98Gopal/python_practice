4c005fd30034258220c39d7320de513ee4e10a2c
manoj98gopal <manoj98gopal@gmail.com>
2024-03-05 06:29:18 +0530

    polymarphism is completed

--- BasicConcepts/oops/polymarphisome.py
+++ BasicConcepts/oops/polymarphisome.py
@@ -1,13 +1,35 @@
 # Polymarphism
 
+# poly means many and marphism means form , something many forms 
 
-class Add:
-    
-    def method(self,a,b):
-        return a+b
 
-class sub:
+class Dog:
     
-    def method(self,a,b):
-        return a-b
+    def getAge(self):
+        print("The Dog age is 5 years")
+        
+        
+class Cat:
     
+    def getAge(self):
+        print("The Cat age is 3 years")
+        
+
+class Human:
+    
+    def getAge(self):
+        print("The human age is 10 years")
+        
+        
+def getAnimalAge(obj):
+    obj.getAge()
+    
+    
+Prem = Human()
+Tommy = Dog()
+Valga = Cat()
+
+
+getAnimalAge(Prem)
+getAnimalAge(Tommy)
+getAnimalAge(Valga)
