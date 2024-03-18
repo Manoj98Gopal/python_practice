a0ce26e594449afe3eab4d20d0ecc8ca85dcb5be
manoj98gopal <manoj98gopal@gmail.com>
2024-03-18 05:58:14 +0530

    makemigration and migration operation completed

--- django_project/storefront/store/models.py
+++ django_project/storefront/store/models.py
@@ -3,19 +3,36 @@ from django.db import models
 # Create your models here.
 
 # creating model, here inheriting the modelsclass of Model method
+
+
+class Collection(models.Model):
+    
+    title = models.CharField(max_length=255)
+    product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name="products_in_collection")
+    
+    
+class Promotions(models.Model):
+    
+    discription = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+
+
 class Product(models.Model):
     
     # creating fields and passing the parameters
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now = True)
-    
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT, related_name="products")
+    promotion = models.ManyToManyField(Promotions)
     
     
 class Customer(models.Model):
-    
     MEMBERSHI_BRONZE = 'B'
     MEMBERSHI_SILVER = 'S'
     MEMBERSHI_GOLD = 'G'
@@ -27,13 +44,12 @@ class Customer(models.Model):
     ]
     
     first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_lenght=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null = True)
     # creating the choice fields and adding the default field
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES,default=MEMBERSHI_BRONZE)
-    
     
     
 class Order(models.Model):
@@ -50,3 +66,31 @@ class Order(models.Model):
     
     place_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1,choices=PAYMENT_CHOICES,default=PAYMENT_PENDING)    
+    customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+    
+
+class OrderItem(models.Model):
+    
+    order = models.ForeignKey(Order,on_delete=models.PROTECT)
+    product = models.ForeignKey(Product,on_delete=models.PROTECT)    
+    quantity = models.PositiveSmallIntegerField()
+    unitprice = models.DecimalField(max_digits=6,decimal_places=2)
+    
+class Address(models.Model):
+    
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    zip = models.CharField(null=False,max_length=20)
+    
+    
+class Cart(models.Model):
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+class CartItem(models.Model):
+    
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
