6280974d519bf4989c4c7e061b0ba35c29505118
manoj98gopal <manoj98gopal@gmail.com>
2025-06-23 21:50:55 +0530

    string methods and input complited

---
+++ NewVersionPath/day1.py
@@ -0,0 +1,58 @@
+# Variables 
+
+name = "Ram Gopal"
+is_developer = True
+age = 50
+rating = 4.5
+
+
+# print(name)
+# print(is_developer)
+# print(age)
+# print(rating)
+
+
+# input taking answer 
+
+# full_name = input("What is you full name? ")
+# color = input("What is your fav color? ")
+
+# message = full_name + " likes " + color
+
+
+# print(message)
+
+
+# calculating age
+
+
+# birth_year = input("Which is you birth year ? ")
+# age = 2025 - int(birth_year)
+
+# print("Your current age is :- ",age)
+
+
+# weight = input("what is your weight tell in pondes ? ")
+
+# result =  float(weight) * 0.45
+
+
+# String 
+
+message = "Python for beginners"
+
+result = message[3:]
+ 
+ 
+# formatted string 
+
+r = f"{name} of this person age is [{age}]"
+
+
+print(len(message))
+print(message.upper())
+print(message.lower())
+print(message.capitalize())
+print(message.find("for"))
+print(message.replace("for","changed"))
+print(message in "for")
