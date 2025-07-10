29c297f5b1fdcae45772262f1bed96aa2198ec32
manoj98gopal <manoj98gopal@gmail.com>
2025-07-10 21:08:31 +0530

    number compliited

---
+++ finalVersion/primitiveTypes/number.py
@@ -0,0 +1,38 @@
+import random
+
+
+x = -5
+y = 34.687684
+z = 2
+
+
+# Absolute value 
+print(abs(x))  #5
+
+# Round to nearest int
+print(round(y))  #35
+
+# Round to 2 decimal 
+print(round(y,2)) #34.69
+
+# Power 
+print(pow(z,3))  # 2 * 2 * 2
+
+# get quotient and remainder 
+print(divmod(3,10))
+
+# get max value
+print(max([1,22,43,3,6]))
+
+# get min value
+print(min([1,22,43,3,6]))
+
+
+# get random number 
+print(random.randint(1, 15))
+
+# ger random choice to list
+print(random.choice([12,45,34,66,22,18]))
+
+
+
